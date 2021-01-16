2b6f265f83d8475654648a653bdd0de42ed32993
TymanWasTaken <32660892+tymanwastaken@users.noreply.github.com>
2021-01-16 01:15:54 -0700

    progress
    please help my brain still hurts

--- src/main/java/com/tyman/skyclientconfig/SkyclientConfig.java
+++ src/main/java/com/tyman/skyclientconfig/SkyclientConfig.java
@@ -4,7 +4,6 @@ import com.tyman.skyclientconfig.commands.DebugCommand;
 import com.tyman.skyclientconfig.confighandlers.DSMHandler;
 import com.tyman.skyclientconfig.confighandlers.ModHandler;
 import com.tyman.skyclientconfig.confighandlers.PatcherHandler;
-import com.tyman.skyclientconfig.utils.Utils;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
@@ -13,7 +12,6 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
 import java.util.HashMap;
-import java.util.Map;
 
 @Mod(modid = com.tyman.skyclientconfig.SkyclientConfig.MODID, version = com.tyman.skyclientconfig.SkyclientConfig.VERSION)
 public class SkyclientConfig
@@ -21,10 +19,6 @@ public class SkyclientConfig
     public static final String MODID = "skyclientconfig";
     public static final String VERSION = "0.0.1";
     public final HashMap<String, ModHandler> configHandlers = new HashMap<>();
-    private static final Map<Class<? extends ModHandler>, String> modHandlers = Utils.createHandlerMap(
-            new Object[]{PatcherHandler.class, "patcher"},
-            new Object[]{DSMHandler.class, "Danker's Skyblock Mod"}
-    );
 
     @Mod.Instance(MODID)
     public static com.tyman.skyclientconfig.SkyclientConfig INSTANCE;
@@ -39,33 +33,15 @@ public class SkyclientConfig
     @SuppressWarnings("unused")
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-        for (Map.Entry<Class<? extends ModHandler>, String> modHandler : modHandlers.entrySet()) {
-            ModHandler handlerClass;
-            try {
-                handlerClass = ModHandler.class.cast(Class.forName("com.tyman.skyclientconfig.confighandlers." + modHandler.getKey()));
-            } catch (ClassNotFoundException e) {
-                System.out.println("Handler class " + modHandler.getKey() + " was not found.");
-                continue;
-            } catch (ClassCastException e) {
-                System.out.println("Unable to cast " + modHandler.getKey() + " to ModHandler");
-                continue;
-            }
-            try {
-                handlerClass = handlerClass.getClass().newInstance();
-            } catch (InstantiationException | IllegalAccessException e) {
-                e.printStackTrace();
-            }
-            if (Loader.isModLoaded(handlerClass.getModid())) {
-                try {
-                    handlerClass.init();
-                } catch (Exception e) {
-                    System.out.println("Unable to initialize handler " + modHandler.getKey());
-                    e.printStackTrace();
-                    continue;
-                }
-                configHandlers.put(handlerClass.getConfigPrefix(), handlerClass);
-                System.out.println("Loaded config handler for mod " + handlerClass.getModid());
-            }
+        if (Loader.isModLoaded("patcher")) {
+            PatcherHandler handler = new PatcherHandler();
+            handler.init();
+            configHandlers.put(handler.getConfigPrefix(), handler);
+        }
+        if (Loader.isModLoaded("Danker's Skyblock Mod")) {
+            DSMHandler handler = new DSMHandler();
+            handler.init();
+            configHandlers.put(handler.getConfigPrefix(), handler);
         }
     }
 }
--- src/main/java/com/tyman/skyclientconfig/confighandlers/DSMHandler.java
+++ src/main/java/com/tyman/skyclientconfig/confighandlers/DSMHandler.java
@@ -1,8 +1,42 @@
 package com.tyman.skyclientconfig.confighandlers;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import me.Danker.handlers.ConfigHandler;
+import net.minecraftforge.common.config.ConfigCategory;
+import net.minecraftforge.common.config.Property;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 public class DSMHandler extends ModHandler {
+
+    public Object parseProp(Property prop) {
+        Object value = null;
+        switch (prop.getType()) {
+            case STRING:
+            case MOD_ID: {
+                value = prop.getString();
+                break;
+            }
+            case INTEGER: {
+                value = prop.getInt();
+                break;
+            }
+            case BOOLEAN: {
+                value = prop.getBoolean();
+                break;
+            }
+            case DOUBLE: {
+                value = prop.getDouble();
+                break;
+            }
+            case COLOR: {
+                throw new IllegalArgumentException("Unable to do colors in dsm handler :/");
+            }
+        }
+        return value;
+    }
+
     @Override
     public String getModid() {
         return "Danker's Skyblock Mod";
@@ -15,16 +49,24 @@ public class DSMHandler extends ModHandler {
 
     @Override
     public void init() {
-        throw new NotImplementedException();
+        // pass, not needed for dsm
     }
 
     @Override
     public Object getConfigProperty(String property) {
-        return null;
+        List<String> split = Arrays.asList(property.split("\\."));
+        if (split.size() != 2) {
+            throw new IllegalArgumentException("DSM config should take one category a property.");
+        }
+        Property prop = ConfigHandler.config.get(split.get(0), split.get(1), (String) null);
+        if (prop == null) {
+            throw new IllegalArgumentException("Could not find config value");
+        }
+        return parseProp(prop);
     }
 
     @Override
     public void setConfigProperty(String prop, Object value) {
-
+        // not made yet
     }
 }
